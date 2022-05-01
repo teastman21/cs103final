0e6559d324468bfca0b1658af5f01a8cdf5fa26f
teastman21 <71243994+teastman21@users.noreply.github.com>
2022-05-01 14:20:42 -0400

    Create Player.js

---
+++ models/Player.js
@@ -0,0 +1,13 @@
+'use strict';
+const mongoose = require( 'mongoose' );
+const Schema = mongoose.Schema;
+const ObjectId = Schema.Types.ObjectId;
+
+var courseSchema = Schema( {
+    firstName: String,
+    lastName: String,
+    playerId: Number,
+    teamId: Number,
+} );
+
+module.exports = mongoose.model( 'Course', courseSchema );
