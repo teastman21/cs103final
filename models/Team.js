7abdf4f806a918b8ac7b53416c55d4697b6d2057
teastman21 <71243994+teastman21@users.noreply.github.com>
2022-05-01 21:04:35 -0400

    Update Team.js

--- models/Team.js
+++ models/Team.js
@@ -3,9 +3,9 @@ const mongoose = require( 'mongoose' );
 const Schema = mongoose.Schema;
 const ObjectId = Schema.Types.ObjectId;
 
-var scheduleSchema = Schema( {
+var teamSchema = Schema( {
   userId: ObjectId,
   playerId: ObjectId,
 } );
 
-module.exports = mongoose.model( 'Team', scheduleSchema );
+module.exports = mongoose.model( 'Team', teamSchema );
